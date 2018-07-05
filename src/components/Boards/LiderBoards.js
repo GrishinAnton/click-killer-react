a866a12a90243077c6e0a1e70202504ea74f963e
Grishin Anton <grishin_anton@mail.ru>
2018-07-05 10:34:17 +0300

    Правка плана

--- src/components/Boards/LiderBoards.js
+++ src/components/Boards/LiderBoards.js
@@ -6,17 +6,47 @@ export default function LiderBoards() {
     return (
         <List>
             <li>
-                <span>id</span>
-                <span>Кол-во</span>
+                <span>1.</span>
+                <span>email</span>
+                <span>20 к/с</span>
+            </li>
+            <li>
+                <span>2.</span>
+                <span>email</span>
+                <span>15 к/с</span>
+            </li>
+            <li>
+                <span>3.</span>
+                <span>email</span>
+                <span>13 к/с</span>
             </li>
         </List>
     )
-
 }
 
 const List = styled.ul`
     list-style-type: none;
     margin: 0;
     padding: 0;
+
+    li {
+        margin-bottom: 10px;
+
+        &:last-of-type {
+            margin-bottom: 0px;
+        }
+    }
+
+    span:first-of-type {
+        margin-right: 10px;
+    }
+
+    span:nth-child(2) {
+        margin-right: 10px;
+    }
+
+    span:nth-child(3) {
+        font-weight: bold;
+    }
 
 `;
